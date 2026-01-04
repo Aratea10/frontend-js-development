05a8af6bc76053a8747c915c1a22146eebb2e134
Sara <saragallegomendez10@gmail.com>
2026-01-04 16:41:23 +0100

    feat: implement basic MVC structure for tweet list by extracting model, view, and controller files

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
-const tweets = [{
-    handler: '@usuario1',
-    date: new Date().toLocaleDateString(),
-    message: 'Eligendi fugiat veniam a neque omnis doloribus sequi porro expedita ullam excepturi.',
-    likes: 40,
-},
-{
-    handler: '@usuario2',
-    date: new Date().toLocaleDateString(),
-    message: 'Laborum ad animi officia dolore nisi necessitatibus a porro! Odit!',
-    likes: 3,
-},
-{
-    handler: '@usuario3',
-    date: new Date().toLocaleDateString(),
-    message: 'Ayer estuve en le parque y me lo pasé genial',
-    likes: 34,
-},
-{
-    handler: '@usuario4',
-    date: new Date().toLocaleDateString(),
-    message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    likes: 400,
-}];
+
+
+function tweetListController(tweetContainer) {
+
+}
 
 
 // 1- recorrer array de tweets - ok
@@ -32,15 +13,15 @@ tweets.forEach((tweet) => {
     newTweet.innerHTML = `
     <h3>${tweet.handler} escribió el ${tweet.date}: </h3>
     <h4>${tweet.message}</h4>
-  `
+    `
 
     const section = document.querySelector('#tweets');
     // 3- pintar cada tweet en pantalla - ok
     section.appendChild(newTweet)
 })
 
-/**
- * recorrer el array de tweets
- * acceder a los datos de cada tweet
- * pintar cada tweet en pantalla
- */
+
+// modelo -> obtención de datos
+// controlador -> orquestar la comunicación entre modelo y vista. Se encarga de gestionar un nodo del DOM.
+// Es el único que interactúa con el DOM
+// vista -> preparación del HTML que vamos a gestionar
