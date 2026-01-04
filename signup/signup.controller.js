891d4a39087c8fe23861f6396db3dd3f2cee3271
Sara <saragallegomendez10@gmail.com>
2026-01-04 17:07:48 +0100

    feat: add client-side signup form handling with a dedicated controller and integrate it into the HTML

---
+++ signup/signup.controller.js
@@ -0,0 +1,10 @@
+export const signupController = (signupForm) => {
+
+
+
+    signupForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        console.log("botón pulsado, toca validar");
+
+    })
+}
