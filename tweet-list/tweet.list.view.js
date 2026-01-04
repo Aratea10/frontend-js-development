3cb123c1305d5e4063a356209e157f4f837f7545
Sara <saragallegomendez10@gmail.com>
2026-01-04 17:02:01 +0100

    feat: add notification display controller and tweet list view builders

--- tweet-list/tweet.list.view.js
+++ tweet-list/tweet.list.view.js
@@ -4,3 +4,7 @@ export const buildTweet = (tweet) => {
     <h4>${tweet.message}</h4>
     `
 }
+
+export const buildEmptyTweets = () => {
+    return `<img style="width: 400px" src="https://compote.slate.com/images/97d7753a-b5ae-408a-8fed-7ff68b4ce3b7.jpeg" />`
+}
