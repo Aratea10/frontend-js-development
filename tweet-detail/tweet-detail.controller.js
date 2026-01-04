619ea8c9401c2d53b59f625a85b37f5d0b3f37a7
Sara <saragallegomendez10@gmail.com>
2026-01-04 17:21:42 +0100

    feat: implement tweet detail feature with dedicated controller and model

--- tweet-detail/tweet-detail.controller.js
+++ tweet-detail/tweet-detail.controller.js
@@ -1,10 +1,15 @@
-import { getTweetDetail } from "./tweet-detail.model.js"
+import { getTweetDetail, getUserData } from "./tweet-detail.model.js"
 import { buildTweetDetail } from "./tweet-detail.view.js"
 
 // user esta imagen para el detalle del tweet si el tweet obtenido por sparrest no tiene imagen.
 // https://demofree.sirv.com/nope-not-here.jpg
 
 export const tweetDetailController = async (tweetDetailContainer, tweetId) => {
+
+    const handleRemoveTweetButton = () => {
+
+    }
+
 
     try {
         const tweet = await getTweetDetail(tweetId)
@@ -14,4 +19,20 @@ export const tweetDetailController = async (tweetDetailContainer, tweetId) => {
         window.location.href = '/'
     }
 
+    try {
+        const userData = await getUserData()
+        handleRemoveTweetButton()
+    } catch (error) {
+
+    }
+
+
+
+
+    /**
+     * obtener datos del usuario que ha iniciado sesión
+     * comparar el id de usuario logado con el id de usuario del tweet
+     * si es el mismo, crear un botón de borrado
+     * cuando el botón se pulsa, confirmar acción y borrar el tweet.
+     */
 }
